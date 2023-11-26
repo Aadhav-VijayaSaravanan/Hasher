1dc1946791d4916cf5631e842aa9b71df1735202
Aadhav Vijaya Saravanan <Aadhavvijayasaravanan@gmail.com>
2023-11-26 09:17:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from faker import Faker
 from hashe import generate_password, hash_password
 from file import create_file, append_to_file
-from pdf import convert_text_to_pdf  # Import the function from pdf_converter.py
+from pdf import convert_text_to_pdf
 
 def generate_user_info(faker, num_users):
     usernames_passwords = [f"{faker.user_name()} {generate_password()}" for _ in range(num_users)]
@@ -12,7 +12,7 @@ def main():
 
     random_user_info_filename = "info.txt"
     hashed_user_info_filename = "hashed.txt"
-    hashed_user_info_pdf_filename = "hashed.pdf"  # New PDF file name
+    hashed_user_info_pdf_filename = "hashed.pdf"
 
     create_file(random_user_info_filename)
     create_file(hashed_user_info_filename)
@@ -27,7 +27,6 @@ def main():
                 hashed_password = hash_password(password)
                 append_to_file(hashed_user_info_filename, f"Username: {username}, Hashed Password: {hashed_password}\n")
 
-        # Convert hashed.txt to PDF
         convert_text_to_pdf(hashed_user_info_filename, hashed_user_info_pdf_filename)
 
 if __name__ == "__main__":
